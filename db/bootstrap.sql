1d4223bb013e7f148d2d8bea092292141934eb1f
Hung Nguyen <hungnguyen10897@gmail.com>
2021-12-10 22:23:00 +0200

    Add 3 Stored Procedure to sync staging data with internal aggregated data

--- db/bootstrap.sql
+++ db/bootstrap.sql
@@ -5,13 +5,14 @@ CREATE DATABASE tha;
 CREATE USER tha_admin PASSWORD 'WJsXNo1TNw';
 GRANT ALL PRIVILEGES ON DATABASE tha TO tha_admin;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO tha_admin;
+GRANT ALL PRIVILEGES ON ALL PROCEDURES IN SCHEMA public TO tha_admin;
 
 DROP TABLE IF EXISTS revenues;
 CREATE TABLE revenues(
     campaign_id INT,
     banner_id INT,
     quarter INT,
-    revenue INT,
+    revenue FLOAT,
     number_of_clicks INT
 )
 
@@ -21,6 +22,10 @@ CREATE EXTERNAL SCHEMA ext_tha_schema FROM DATA CATALOG
 DATABASE 'hung_ext_tha_db' 
 iam_role 'arn:aws:iam::939595455984:role/hung-redshift-spectrum'
 CREATE EXTERNAL DATABASE IF NOT EXISTS;
+
+--GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA ext_tha_schema TO tha_admin;
+GRANT USAGE ON SCHEMA ext_tha_schema to tha_admin;
+ALTER SCHEMA ext_tha_schema OWNER TO tha_admin;
 
 -- Impressions
 DROP TABLE IF EXISTS ext_tha_schema.impressions;
@@ -73,7 +78,7 @@ PARTITION (quarter= 3) LOCATION 's3://hungthas3/conversions/quarter=3/'
 PARTITION (quarter= 4) LOCATION 's3://hungthas3/conversions/quarter=4/';
 
 -- POPULATE INTERNAL AGGREGATED TABLE
-TRUNCATE TABLE revenues
+TRUNCATE TABLE revenues;
 INSERT INTO revenues
 SELECT
     impressions.campaign_id AS campaign_id,
@@ -87,23 +92,215 @@ SELECT
   		WHEN COUNT(clicks.click_id) IS NOT NULL THEN COUNT(clicks.click_id)
   		ELSE 0
     END AS number_of_clicks
-FROM
-  (
-    SELECT * FROM ext_tha_schema.impressions 
-  ) AS impressions
-  LEFT JOIN
-  (
-    SELECT * FROM ext_tha_schema.clicks
-  ) AS clicks
-  ON
-  	impressions.banner_id = clicks.banner_id
+FROM ext_tha_schema.impressions AS impressions
+LEFT JOIN ext_tha_schema.clicks AS clicks
+  ON impressions.banner_id = clicks.banner_id
   	AND impressions.campaign_id = clicks.campaign_id
     AND impressions.quarter = clicks.quarter
-  LEFT JOIN 
-  (
-    SELECT * FROM ext_tha_schema.conversions
-  ) AS conversions
+LEFT JOIN  ext_tha_schema.conversions AS conversions
   ON clicks.click_id = conversions.click_id
   	AND conversions.quarter = clicks.quarter
 GROUP BY impressions.campaign_id, impressions.banner_id, impressions.quarter;
+
+-- CREATE STORED PROCEDURES
+CREATE OR REPLACE PROCEDURE public.synch_new_impressions
+(
+  ext_table text,
+  quarter text
+)
+AS $$
+BEGIN
+
+    DROP TABLE IF EXISTS temp;
+    
+    -- Create and Populate Temp View
+    -- Temp View aggregates data from staging table and current 'revenues'
+    EXECUTE(
+       'CREATE TEMP TABLE temp AS '
+    +  '( '
+    +    'SELECT '
+    +      'campaign_id, '
+    +      'banner_id, '
+    +      'quarter, '
+    +      'SUM(revenue) AS revenue, '
+    +      'SUM(number_of_clicks) AS number_of_clicks '
+    +    'FROM ' 
+    +    '( '
+    +      'SELECT '
+    +        'impressions.campaign_id AS campaign_id, '
+    +        'impressions.banner_id AS banner_id, '
+    +        quarter || ' AS quarter, '
+    +        'CASE '
+    +            'WHEN SUM(conversions.revenue) IS NOT NULL THEN SUM(conversions.revenue) '
+    +            'ELSE 0 '
+    +        'END AS revenue, '
+    +        'CASE '
+    +            'WHEN COUNT(clicks.click_id) IS NOT NULL THEN COUNT(clicks.click_id) '
+    +            'ELSE 0 '
+    +        'END AS number_of_clicks '
+    +      'FROM ( '
+    +        'SELECT * FROM ' || ext_table
+    +      ') AS impressions '
+    +      'LEFT JOIN '
+    +        '( '
+    +          'SELECT * FROM ext_tha_schema.clicks WHERE quarter= ' || quarter
+    +        ') AS clicks '
+    +      'ON impressions.banner_id = clicks.banner_id '
+    +         'AND impressions.campaign_id = clicks.campaign_id '
+    +      'LEFT JOIN '
+    +        '( '
+    +          'SELECT * FROM ext_tha_schema.conversions WHERE quarter= ' || quarter
+    +        ') AS conversions '
+    +      'ON clicks.click_id = conversions.click_id '
+    +      'GROUP BY impressions.campaign_id, impressions.banner_id '
+    +      'UNION ALL '
+    +      'SELECT * FROM revenues '
+    +    ') '
+    +    'GROUP BY campaign_id, banner_id, quarter '
+    +  '); '
+	);
+    
+    -- Replace revenues with temp
+    TRUNCATE TABLE revenues;
+
+	INSERT INTO revenues SELECT * FROM temp;
+    
+    DROP TABLE temp;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE PROCEDURE public.synch_new_clicks
+(
+  ext_table text,
+  quarter text
+)
+AS $$
+BEGIN
+
+    DROP TABLE IF EXISTS temp;
+    
+    -- Create and Populate Temp View
+    -- Temp View aggregates data from staging table and current 'revenues'
+    EXECUTE(
+       'CREATE TEMP TABLE temp AS '
+    +  '( '
+    +    'SELECT '
+    +      'campaign_id, '
+    +      'banner_id, '
+    +      'quarter, '
+    +      'SUM(revenue) AS revenue, '
+    +      'SUM(number_of_clicks) AS number_of_clicks '
+    +    'FROM ' 
+    +    '( '
+    +      'SELECT '
+    +        'impressions.campaign_id AS campaign_id, '
+    +        'impressions.banner_id AS banner_id, '
+    +        quarter || ' AS quarter, '
+    +        'CASE '
+    +            'WHEN SUM(conversions.revenue) IS NOT NULL THEN SUM(conversions.revenue) '
+    +            'ELSE 0 '
+    +        'END AS revenue, '
+    +        'CASE '
+    +            'WHEN COUNT(clicks.click_id) IS NOT NULL THEN COUNT(clicks.click_id) '
+    +            'ELSE 0 '
+    +        'END AS number_of_clicks '
+    +      'FROM ( '
+    +        'SELECT * FROM ext_tha_schema.impressions WHERE quarter= ' || quarter
+    +      ') AS impressions '
+    +      'LEFT JOIN '
+    +        '( '
+    +          'SELECT * FROM ' || ext_table
+    +        ') AS clicks '
+    +      'ON impressions.banner_id = clicks.banner_id '
+    +         'AND impressions.campaign_id = clicks.campaign_id '
+    +      'LEFT JOIN '
+    +        '( '
+    +          'SELECT * FROM ext_tha_schema.conversions WHERE quarter= ' || quarter
+    +        ') AS conversions '
+    +      'ON clicks.click_id = conversions.click_id '
+    +      'GROUP BY impressions.campaign_id, impressions.banner_id '
+    +      'UNION ALL '
+    +      'SELECT * FROM revenues '
+    +    ') '
+    +    'GROUP BY campaign_id, banner_id, quarter '
+    +  '); '
+	);
+    
+    -- Replace revenues with temp
+    TRUNCATE TABLE revenues;
+
+	INSERT INTO revenues SELECT * FROM temp;
+    
+    DROP TABLE temp;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE PROCEDURE public.synch_new_conversions
+(
+  ext_table text,
+  quarter text
+)
+AS $$
+BEGIN
+
+    DROP TABLE IF EXISTS temp;
+    
+    -- Create and Populate Temp View
+    -- Temp View aggregates data from staging table and current 'revenues'
+    EXECUTE(
+       'CREATE TEMP TABLE temp AS '
+    +  '( '
+    +    'SELECT '
+    +      'campaign_id, '
+    +      'banner_id, '
+    +      'quarter, '
+    +      'SUM(revenue) AS revenue, '
+    +      'SUM(number_of_clicks) AS number_of_clicks '
+    +    'FROM ' 
+    +    '( '
+    +      'SELECT '
+    +        'impressions.campaign_id AS campaign_id, '
+    +        'impressions.banner_id AS banner_id, '
+    +        quarter || ' AS quarter, '
+    +        'CASE '
+    +            'WHEN SUM(conversions.revenue) IS NOT NULL THEN SUM(conversions.revenue) '
+    +            'ELSE 0 '
+    +        'END AS revenue, '
+    +        'CASE '
+    +            'WHEN COUNT(clicks.click_id) IS NOT NULL THEN COUNT(clicks.click_id) '
+    +            'ELSE 0 '
+    +        'END AS number_of_clicks '
+    +      'FROM ( '
+    +        'SELECT * FROM ext_tha_schema.impressions WHERE quarter= ' || quarter
+    +      ') AS impressions '
+    +      'LEFT JOIN '
+    +        '( '
+    +          'SELECT * FROM ext_tha_schema.clicks WHERE quarter= ' || quarter
+    +        ') AS clicks '
+    +      'ON impressions.banner_id = clicks.banner_id '
+    +         'AND impressions.campaign_id = clicks.campaign_id '
+    +      'LEFT JOIN '
+    +        '( '
+    +          'SELECT * FROM ' || ext_table
+    +        ') AS conversions '
+    +      'ON clicks.click_id = conversions.click_id '
+    +      'GROUP BY impressions.campaign_id, impressions.banner_id '
+    +      'UNION ALL '
+    +      'SELECT * FROM revenues '
+    +    ') '
+    +    'GROUP BY campaign_id, banner_id, quarter '
+    +  '); '
+	);
+    
+    -- Replace revenues with temp
+    TRUNCATE TABLE revenues;
+
+	INSERT INTO revenues SELECT * FROM temp;
+    
+    DROP TABLE temp;
+END;
+$$ LANGUAGE plpgsql;
+
 
